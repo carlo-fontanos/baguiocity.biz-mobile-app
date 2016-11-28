92d77dd7ebabefdb2880f3a00ccfcb133ce2a13e
Carlo Fontanos <carl.fontanos@gmail.com>
2016-11-28 19:05:13 +0800

    Integrated gallery view, title and price to the single item view. Created a new API that will load data based on item id parameter using fetch method.

--- Pages/ListingsPageSingle.js
+++ Pages/ListingsPageSingle.js
@@ -1,24 +1,23 @@
+var Observable = require("FuseJS/Observable");
+
 var item = this.Parameter;
 
-var ID = item.map(function(x) { return x.ID; });
-var title = item.map(function(x) { return x.title; });
-var content = item.map(function(x) { return x.content; });
-var author = item.map(function(x) { return x.author; });
-var date = item.map(function(x) { return x.date; });
-var condition = item.map(function(x) { return x.condition; });
-var price = item.map(function(x) { return x.price; });
-var image = item.map(function(x) { return x.image; });
-var gallery = item.map(function(x) { return x.gallery; });
+var data = item.map(function(x) {
+	var result = Observable();
+	
+	fetch('http://baguiocity.biz/api/single-item.php?id=' + x.item_id)
+		.then(function(response) { 
+			return response.json(); 
+		})
+		.then(function(responseObject) {
+			result.value = responseObject;
+		});
+		
+	return result;
+});
 
+	
 /* Make our code acciessuble in XML */
 module.exports = {
-	ID: ID,
-	title: title,
-	content: content,
-	author: author,
-	date: date,
-	condition: condition,
-	price: price,
-	image: image,
-	gallery: gallery,
+	data: data
 };
